f396ae40fb28650f6162cbdef0093acd8395d71b
yasmeensaber <yasmeensaber94@gmail.com>
2017-11-15 16:33:53 -0800

    testing

---
+++ DissolveEffect/Assets/Scripts/DisolveEffectTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisolveEffectTrigger : MonoBehaviour {
+
+	public Material disolveMatrial;
+	public float speed,max;
+	private float currentY, startTime;
+	private void Update()
+	{
+		if (currentY < max) {
+			disolveMatrial.SetFloat ("_DisolveY",currentY);
+			currentY += Time.deltaTime * speed;
+
+		}
+		if (Input.GetKeyDown (KeyCode.E)) {
+			TriggerEffect ();
+		}
+	}
+	private void TriggerEffect ()
+	{
+		startTime = Time.time;
+		currentY = 0;
+	}
+}
